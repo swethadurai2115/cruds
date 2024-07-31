016a5158d3fe66870007054f854af32d71c79889
swethadurai2115 <swethadurai2115@gmail.com>
2024-07-31 17:39:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,189 @@
-import { useEffect, useState  } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [filterusers, setFilterusers] = useState([])
+  const [filterusers, setFilterusers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userData, setUserData] = useState({ name: "", age: "", city: "" });
+
   const getAllUsers = async () => {
-    await axios.get("http://localhost:8000/users").then
-    ((res) => {
+    await axios.get("http://localhost:8000/users").then((res) => {
       setUsers(res.data);
       setFilterusers(res.data);
     });
-  }
-  useEffect(() => {
-   getAllUsers();
-  }, []);
- //search function
- const handleSearchChange = (e) => {
-  const SearchText=e.target.value.toLowerCase();
-  const filterusers = users.filter((user) => user.name.
-  toLowerCase().includes(SearchText)) || users.city.
-  toLowercase().includes(SearchText);
- };
-return (
-  <>
-    <div className="container">
-       <h3>CRUD Application</h3>
-       <div className="input-search">
-      <input type="search" placeholder="Search Text Here" onChange={handleSearchChange}/>
-     <button className="btn green">Add Record</button>
+  };
 
-<table className="table">
- <thead>
-   <tr>
-       <th>Id</th>
-       <th>Name</th>
-       <th>Age</th>
-       <th>City</th>
-       <th>Edit</th>
-      <th>Delete</th>
-   </tr>
- </thead>
- <tbody>
- {users && 
-    users.map((user, index)=> {
-    return(
-        <tr key={user.id}>
-           <td>{index + 1}</td>
-           <td>{user.name}</td>
-           <td>{user.age}</td>
-           <td>{user.city}</td>
-        <td>
-          <button className="btn green">Edit</button></td>
-          <td><button className="btn red">Delete</button></td>
-          </tr>         
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  // Search function
+  const handleSearchChange = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    const filteredUsers = users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(searchText) ||
+        user.city.toLowerCase().includes(searchText)
+    );
+    setFilterusers(filteredUsers);
+  };
+
+  // Delete function
+  const handleDelete = async (id) => {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this user?"
+    );
+
+    if (isConfirmed) {
+      await axios.delete(`http://localhost:8000/users/${id}`).then((res) => {
+        setUsers(res.data);
+        setFilterusers(res.data);
+      });
+    }
+  };
+
+  // Close modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+    getAllUsers();
+  };
+
+  // Add user details
+  const handleAddRecord = () => {
+    setUserData({ name: "", age: "", city: "" });
+    setIsModalOpen(true);
+  };
+
+  const handleData = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (userData.id) {
+      await axios
+        .patch(`http://localhost:8000/users/${userData.id}`, userData)
+        .then((res) => {
+          console.log(res);
+        });
+    } else {
+      await axios.post("http://localhost:8000/users", userData).then((res) => {
+        console.log(res);
+      });
+    }
+    closeModal();
+    setUserData({ name: "", age: "", city: "" });
+  };
+
+  // Update user function
+  const handleUpdateRecord = (user) => {
+    setUserData(user);
+    setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h3>CRUD Application</h3>
+        <div className="input-search">
+          <input
+            type="search"
+            placeholder="Search Text Here"
+            onChange={handleSearchChange}
+          />
+          <button className="btn green" onClick={handleAddRecord}>
+            Add Record
+          </button>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterusers &&
+              filterusers.map((user, index) => {
+                return (
+                  <tr key={user.id}>
+                    <td>{index + 1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                    <td>{user.city}</td>
+                    <td>
+                      <button
+                        className="btn green"
+                        onClick={() => handleUpdateRecord(user)}
+                      >
+                        Edit
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => handleDelete(user.id)}
+                        className="btn red"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>
+                &times;
+              </span>
+              <h2>{userData.id ? "User Record" : "Add Record"} </h2>
+              <div className="input-group">
+                <label htmlFor="name">Full Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={userData.name}
+                  id="name"
+                  onChange={handleData}
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="name">Age</label>
+                <input
+                  type="number"
+                  name="age"
+                  value={userData.age}
+                  id="age"
+                  onChange={handleData}
+                />
+              </div>
+              <div className="input-group">
+                <label htmlFor="name">City</label>
+                <input
+                  type="text"
+                  name="city"
+                  value={userData.city}
+                  id="city"
+                  onChange={handleData}
+                />
+              </div>
+              <button className="btn green" onClick={handleSubmit}>
+                {userData.id ? "Update User" : "Add User"}
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
- })}
-    </tbody>
- </table>
-</div>
-</div>
-</>
-);
 }
+
 export default App;
